1733585282f2112a09d492a94ff3a2132ad58a9c
Salman-S-Rahman <salmansr.developer@gmail.com>
2021-12-11 19:50:56 +0600

    added

---
+++ math.js/math.js
@@ -0,0 +1,10 @@
+console.log(Math.sqrt(25));
+console.log(Math.abs(-2.000001));
+console.log(Math.floor(2.999));
+console.log(Math.ceil(2.0001));
+console.log(Math.round(3.5));
+console.log(Math.round(3.4));
+console.log(Math.ceil(Math.random() * 5));
+console.log(Math.round(Math.random() * 5));
+console.log(Math.round(Math.random() * 9) + 1);
+console.log(Math.floor(Math.random() * 10) + 1)
